fab762b46d4cbacca2df4c91a00e379e05cca965
Ross Coombs <coombs.ross@gmail.com>
2025-08-19 16:51:16 +0100

    published version 1908

---
+++ wwwroot/tts.js
@@ -0,0 +1,12 @@
+﻿function speakText(text)
+{
+    // Check if the browser supports the Speech Synthesis API
+    if ('speechSynthesis' in window) {
+        var utterance = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(utterance);
+    } else
+    {
+        console.error('This browser does not support the Speech Synthesis API.');
+        alert('Sorry, your browser does not support text-to-speech!\nUse another browser and try again!');
+    }
+}
